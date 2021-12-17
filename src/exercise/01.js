3c54a40291f4a93bdb7aca5c7fc82030141a379b
soufiane chaoufi <soufiane.chaoufi@gmail.com>
2021-12-17 08:16:37 -0800

    traditional dispatch object

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,18 +4,20 @@
 import * as React from 'react'
 
 const countReducer = (state,action) => {
-  return {
-    ...state,
-    ...(typeof action === 'function' ? action(state) : action)
-  }
+  switch (action.type) {
+    case 'INCREMENT':
+      return { count: state.count + action.step }
+    default:
+      return state
+}
 }
 function Counter({initialCount = 0, step = 1}) {
-  const [state, setState] = React.useReducer(countReducer, {
+  const [state, dispatch] = React.useReducer(countReducer, {
     count: initialCount,
   })
   const { count } = state
-  const increment = () => setState((currentState)=>({count: currentState.count+step}))
-  return <button onClick={increment}>{count}</button>
+const increment = () => dispatch({type: 'INCREMENT', step})
+return <button onClick={increment}>{count}</button>
 }
 
 function App() {
